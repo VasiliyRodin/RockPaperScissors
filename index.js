4a77bd057b5e428ac88f2d5ecb0c78409764d50a
Vasiliy rodin <vasiliyrodin@gmail.com>
2025-08-01 13:15:00 -0700

    create different logic for comparison each case of players choice gets compared

--- index.js
+++ index.js
@@ -27,14 +27,44 @@ function game(){
 //condition1 computer choice is paper: computer wins restart game()
 //condition2 computer choice is scissor: player wins restart game()
 //condition3 computer choice is rock: its a tie restart game()
-    switch(playerChoice=="rock"){
-        case computerChoice === "rock":
-            console.log("Computer chose rock its a tie");
-        case computerChoice === "paper":
-            console.log("Computer chose paper it wins");
-        case computerChoice === "scissors":
-            console.log("Computer chose scissors player wins")
-    }
+    switch (playerChoice) {
+        case "rock":
+            if (computerChoice === "paper"){
+                console.log("Computer chose paper, computer wins");
+                break;
+            }else if (computerChoice === "scissors"){
+                console.log("Computer chose scissors, player wins");
+                break;
+            }
+            else{
+                console.log("Computer chose rock, its a tie");
+                break;
+            }
+        case "paper":
+            if (computerChoice === "scissors"){
+                console.log("Computer scissors , computer wins")
+                break;
+            }else if (computerChoice === "rock"){
+                console.log("Computer chose rock, player wins");
+                break;
+            }
+            else{
+                console.log("Computer chose paper, its a tie");
+                break;
+            }
+        case "scissors":
+            if (computerChoice === "rock"){
+                console.log("Computer chose rock, computer wins");
+                break;
+            }else if (computerChoice === "paper"){
+                console.log("Computer chose paper, player wins");
+                break;
+            }
+            else{
+                console.log("Computer chose scissors, its a tie");
+                break;
+            }
+    }    
 }
 game();
 
