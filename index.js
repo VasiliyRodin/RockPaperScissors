55d4256d03fd42b3a549d3236ee6d996245aeec0
Vasiliy rodin <vasiliyrodin@gmail.com>
2025-08-01 12:28:26 -0700

    write pseudo code for game logic

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+/* 
+create a function of game() which will have game logic
+
+ask user for their choice of rock paper scissor
+store that in a value playerChoice
+
+get computer choice by running random gen 1-3 1rock 2 paper 3 scissor
+store that in computerChoice
+
+Compare choices using case
+case1 userChoice is rock
+condition1 computer choice is paper: computer wins restart game()
+condition2 computer choice is scissor: player wins restart game()
+condition3 computer choice is rock: its a tie restart game()
+ */
