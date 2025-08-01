e2aeb017d02d055323b5a20179f5b793ba190d69
Vasiliy rodin <vasiliyrodin@gmail.com>
2025-08-01 12:48:00 -0700

    Add game function Player choice store player choice let computer choose between 1-3 store computer choice compare logic for playerChoice is rock

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-/* 
-create a function of game() which will have game logic
 
-ask user for their choice of rock paper scissor
-store that in a value playerChoice
+//create a function of game() which will have game logic
+function game(){
+//ask user for their choice of rock paper scissor
+//store that in a value playerChoicefunction game(){
+    const playerChoice = prompt("Choose rock paper or scissors");
+    console.log(playerChoice);
 
-get computer choice by running random gen 1-3 1rock 2 paper 3 scissor
-store that in computerChoice
+//get computer choice by running random gen 1-3 1rock 2 paper 3 scissor
+//store that in computerChoice
+    let computerChoice="";
+// translate computerChoice to rock paper or scissors.
+    switch(Math.floor(Math.random() *3)+1){
+        case 1:
+            computerChoice = "rock";
+            break;
+        case 2:
+            computerChoice = "paper";
+            break;
+        case 3:
+            computerChoice = "scissors";
+            break;            
+    }
+    console.log(computerChoice);
+//Compare choices using case
+//case1 userChoice is rock
+//condition1 computer choice is paper: computer wins restart game()
+//condition2 computer choice is scissor: player wins restart game()
+//condition3 computer choice is rock: its a tie restart game()
+    switch(playerChoice=="rock"){
+        case computerChoice === "rock":
+            console.log("Computer chose rock its a tie");
+        case computerChoice === "paper":
+            console.log("Computer chose paper it wins");
+        case computerChoice === "scissors":
+            console.log("Computer chose scissors player wins")
+    }
+}
+game();
 
-Compare choices using case
-case1 userChoice is rock
-condition1 computer choice is paper: computer wins restart game()
-condition2 computer choice is scissor: player wins restart game()
-condition3 computer choice is rock: its a tie restart game()
- */
